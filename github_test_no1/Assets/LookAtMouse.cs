5202f208782d5bf3ea99a6d54656ce4b4abad1bd
jbala50 <jbala50@hotmail.es>
2021-09-22 12:48:03 +0200

    Añadido seguimiento de raton y espada

---
+++ github_test_no1/Assets/LookAtMouse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+    [SerializeField]
+    Camera mainCamera;
+
+    private void Start()
+    {
+
+        if (mainCamera == null)
+            mainCamera = Camera.main;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0;
+        Vector3 lookAtDirection = mouseWorldPosition - target.position;
+        target.right = lookAtDirection;
+    }
+}
